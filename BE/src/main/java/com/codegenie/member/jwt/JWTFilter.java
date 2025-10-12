eed579f80348e096803a2bd7d93a8f4caebdbb86
kks1177 <kks1177.dev@gmail.com>
2025-10-13 00:59:28 +0900

    Minor: NonNull 경고 제거(어노테이션 추가)

--- BE/src/main/java/com/codegenie/member/jwt/JWTFilter.java
+++ BE/src/main/java/com/codegenie/member/jwt/JWTFilter.java
@@ -2,6 +2,7 @@ package com.codegenie.member.jwt;
 
 import java.io.IOException;
 
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,9 +30,9 @@ public class JWTFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                   HttpServletResponse response,
-                                   FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                   @NonNull HttpServletResponse response,
+                                   @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
